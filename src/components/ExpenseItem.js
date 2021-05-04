276352426448cbd781080c9104ddfdb9ab98fc0f
zabroski <35234595+zabroski@users.noreply.github.com>
2021-05-03 21:33:21 -0400

    working on date and prosps

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const mounth = props.date.toLocaleString('en-US', { month: 'long'})
+    const day = props.date.toLocaleString('en-Us', { day: '2-digit'});
+    const year = props.date.getFullYear();
+  
     return (
         <div className="expense-item">
-            <div>May 3th 2021</div>
+            <div>
+                <div>{mounth}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
-                <h2>Rent</h2>
-                <div className="expense-item__price">$2732.56</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
 
