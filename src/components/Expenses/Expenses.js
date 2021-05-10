1632dbebc14488445befe0bb14f3266b6f018910
zabroski <35234595+zabroski@users.noreply.github.com>
2021-05-10 09:00:49 -0400

    it monday work work

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,6 +12,23 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear)
   }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if (expensesContent.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
   return (
     <div>
       <Card className='expenses'>
@@ -19,13 +36,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+       {expensesContent}
       </Card>
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -28,7 +28,6 @@ const ExpenseForm = (props) => {
             amount: enteredAmount,
             date: new Date(enteredDate),    
     };
-    console.log(expenseData)
     props.onSaveExpenseData(expenseData)
     setEnteredTitle('');
     setEnteredAmount('');
